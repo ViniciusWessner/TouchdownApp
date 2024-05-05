7334a01b293e190453be07fea4f5faaa9aef24cb
Vinicius wessner <67404041+ViniciusWessner@users.noreply.github.com>
2024-05-04 23:04:13 -0300

    Project created and configuration base

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,8 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Vinicius Wessner on 04/05/24.
+//
+
+import Foundation
---
+++ Touchdown/View/Home/FooterView.swift
@@ -0,0 +1,18 @@
+//
+//  FooterView.swift
+//  Touchdown
+//
+//  Created by Vinicius Wessner on 04/05/24.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FooterView()
+}
