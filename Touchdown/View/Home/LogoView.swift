8323a573fd59affa0902b76cc8c6bde9b81c8f80
Vinicius wessner <67404041+ViniciusWessner@users.noreply.github.com>
2024-05-06 09:31:39 -0300

    Created navigation bar view and config contentview

---
+++ Touchdown/View/Home/LogoView.swift
@@ -0,0 +1,18 @@
+//
+//  LogoView.swift
+//  Touchdown
+//
+//  Created by Vinicius Wessner on 06/05/24.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogoView()
+}
---
+++ Touchdown/View/Home/NavigationBarView.swift
@@ -0,0 +1,54 @@
+//
+//  NavigationBarTitle.swift
+//  Touchdown
+//
+//  Created by Vinicius Wessner on 06/05/24.
+//
+
+import SwiftUI
+
+struct NavigationBarTitle: View {
+    
+    @State private var isAnimated: Bool = false
+    
+    var body: some View {
+        HStack {
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image(systemName: "magnifyingglass")
+                    .font(.title)
+                    .foregroundStyle(.black)
+            })
+            
+            Spacer()
+            
+            LogoView()
+                .opacity(isAnimated ? 1 : 0)
+                .offset(x: 0, y: isAnimated ? 0 : -25)
+                .onAppear(perform: {
+                    withAnimation(.easeOut(duration: 0.5)) {
+                        isAnimated.toggle()
+                    }
+                })
+            
+            
+            Spacer()
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                ZStack {
+                    Image(systemName: "cart")
+                        .font(.title)
+                    .foregroundStyle(.black)
+                    
+                    Circle()
+                        .fill(Color.red)
+                        .frame(width: 14, height: 14, alignment: .center)
+                        .offset(x: 13, y: -10)
+                }
+            })
+        }
+    }
+}
+
+#Preview {
+    NavigationBarTitle()
+}
